1a423532747863ac8f4855900d294d100fa894d2
Artem <popov2202@gmail.com>
2018-10-24 23:55:10 +0300

    database creation script updated

--- script.sql
+++ script.sql
@@ -1,69 +1,120 @@
---  Database `promo` -- 
 
-
+DROP  DATABASE IF EXISTS promo;
 CREATE DATABASE promo;
+DROP TABLE IF EXISTS `promo`.`promoComments`;
 
 
--- Table `promo`.`Users` -- 
-
-
-
-
-DROP TABLE IF EXISTS `promo`.`Users`;
-		
-CREATE TABLE `promo`.`Users` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `login` VARCHAR(177) NULL DEFAULT NULL,
-  `email` VARCHAR(255) NULL DEFAULT NULL,
-  `password` VARCHAR(100) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY (`email`)
-) ENGINE=InnoDB CHARACTER SET=UTF8;
-
-
--- Table `promo`.`promocodes` -- 
+DROP TABLE IF EXISTS `promo`.`shopComments`;
 
 
 DROP TABLE IF EXISTS `promo`.`promocodes`;
-		
-CREATE TABLE `promo`.`promocodes` (
-  `promoId` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `promo` VARCHAR(100) NULL DEFAULT NULL,
-  `site` VARCHAR(100) NULL DEFAULT NULL,
-  `userId` INTEGER NULL DEFAULT NULL,
-  `issueDate` DATE NULL DEFAULT NULL,
-  `likes` INTEGER NULL DEFAULT 0,
-  `dislikes` INTEGER NULL DEFAULT 0,
-  PRIMARY KEY (`promoId`),
-  UNIQUE KEY (`promo`),
-  FOREIGN KEY (`userId`) REFERENCES Users(`id`)
-) ENGINE=InnoDB CHARACTER SET=UTF8;
 
 
--- Table `promo`.`comments` -- 
+DROP TABLE IF EXISTS `promo`.`users`;
 
 
-DROP TABLE IF EXISTS `promo`.`comments`;
-		
-CREATE TABLE `promo`.`comments` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `userId` INTEGER NULL DEFAULT NULL,
-  `promoId` INTEGER NULL DEFAULT NULL,
-  `comment` MEDIUMTEXT NULL DEFAULT NULL,
-  `time` DATETIME NULL DEFAULT NULL,
+DROP TABLE IF EXISTS `promo`.`groups`;
+
+
+DROP TABLE IF EXISTS `promo`.`shops`;
+
+
+
+-- ************************************** `groups`
+
+CREATE TABLE `promo`.`groups`
+(
+  `id`   INTEGER NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(45) NOT NULL ,
+
+  PRIMARY KEY (`id`)
+);
+
+
+
+-- ************************************** `shops`
+
+CREATE TABLE `promo`.`shops`
+(
+  `id`       INTEGER NOT NULL AUTO_INCREMENT ,
+  `name`     VARCHAR(100) NOT NULL ,
+  `URL`      VARCHAR(100) NOT NULL ,
+  `count`    INTEGER NOT NULL ,
+  `likes`    INTEGER NOT NULL ,
+  `dislikes` INTEGER NOT NULL ,
+
+  PRIMARY KEY (`id`)
+);
+
+
+
+-- ************************************** `users`
+
+CREATE TABLE `promo`.`users`
+(
+  `id`       INTEGER NOT NULL AUTO_INCREMENT ,
+  `login`    VARCHAR(177) NOT NULL ,
+  `email`    VARCHAR(255) NOT NULL ,
+  `password` VARCHAR(100) NOT NULL ,
+  `rating`   INTEGER NOT NULL ,
+  `groupID`  INTEGER NOT NULL ,
+
   PRIMARY KEY (`id`),
-  FOREIGN KEY (`userId`) REFERENCES Users(`id`),
-  FOREIGN KEY (`promoId`) REFERENCES promocodes(`promoId`)
-) ENGINE=InnoDB CHARACTER SET=UTF8;
+  FOREIGN KEY (`groupID`) REFERENCES `groups` (`id`)
+);
 
 
 
--- Test Data -- 
+-- ************************************** `shopComments`
+
+CREATE TABLE `promo`.`shopComments`
+(
+  `id`      INTEGER NOT NULL AUTO_INCREMENT ,
+  `comment` MEDIUMTEXT NOT NULL ,
+  `time`    DATETIME NOT NULL ,
+  `userID`  INTEGER NOT NULL ,
+  `shopID`  INTEGER NOT NULL ,
+
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`userID`) REFERENCES `users` (`id`),
+  FOREIGN KEY (`shopID`) REFERENCES `shops` (`id`)
+);
 
 
--- INSERT INTO `Users` (`id`,`login`,`email`,`password`) VALUES
--- ('','','','');
--- INSERT INTO `promocodes` (`promoId`,`promo`,`userId`,`issueDate`,`likes`,`dislikes`) VALUES
--- ('','','','','','');
--- INSERT INTO `comments` (`id`,`userId`,`promoId`,`comment`,`time`) VALUES
--- ('','','','','');
+
+
+-- ************************************** `promocodes`
+
+CREATE TABLE `promo`.`promocodes`
+(
+  `PromoID`     INTEGER NOT NULL AUTO_INCREMENT ,
+  `promo`       VARCHAR(100) ,
+  `issueDate`   DATE NOT NULL ,
+  `expireDate`  DATE NOT NULL ,
+  `likes`       INTEGER NOT NULL ,
+  `dislikes`    INTEGER NOT NULL ,
+  `description` MEDIUMTEXT NOT NULL ,
+  `userID`      INTEGER NOT NULL ,
+  `shopId`      INTEGER NOT NULL ,
+
+  PRIMARY KEY (`PromoID`),
+  FOREIGN KEY (`userID`) REFERENCES `users` (`id`),
+  FOREIGN KEY (`shopId`) REFERENCES `shops` (`id`)
+);
+
+
+
+-- ************************************** `promoComments`
+
+CREATE TABLE `promo`.`promoComments`
+(
+  `id`      INTEGER NOT NULL AUTO_INCREMENT ,
+  `comment` MEDIUMTEXT NOT NULL ,
+  `time`    DATETIME NOT NULL ,
+  `userID`  INTEGER NOT NULL ,
+  `promoID` INTEGER NOT NULL ,
+
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`userID`) REFERENCES `users` (`id`),
+  FOREIGN KEY (`promoID`) REFERENCES `promocodes` (`PromoID`)
+);
