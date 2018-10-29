3fa40247b7c2f6070d39fe959f4c36587617c0be
Artem <popov2202@gmail.com>
2018-10-30 01:08:03 +0300

    database creation script updated

--- script.sql
+++ script.sql
@@ -1,23 +1,18 @@
 
-DROP  DATABASE IF EXISTS promo;
+-- DROP  DATABASE IF EXISTS promo;
+
 CREATE DATABASE promo;
+
+DROP TABLE IF EXISTS `promo`.`promoDislikes`;
+DROP TABLE IF EXISTS `promo`.`promoLikes`;
+DROP TABLE IF EXISTS `promo`.`shopDislikes`;
+DROP TABLE IF EXISTS `promo`.`shopLikes`;
 DROP TABLE IF EXISTS `promo`.`promoComments`;
-
-
 DROP TABLE IF EXISTS `promo`.`shopComments`;
-
-
 DROP TABLE IF EXISTS `promo`.`promocodes`;
-
-
 DROP TABLE IF EXISTS `promo`.`users`;
-
-
 DROP TABLE IF EXISTS `promo`.`groups`;
-
-
 DROP TABLE IF EXISTS `promo`.`shops`;
-
 
 
 -- ************************************** `groups`
@@ -37,11 +32,9 @@ CREATE TABLE `promo`.`groups`
 CREATE TABLE `promo`.`shops`
 (
   `id`       INTEGER NOT NULL AUTO_INCREMENT ,
-  `name`     VARCHAR(100) NOT NULL ,
-  `URL`      VARCHAR(100) NOT NULL ,
-  `count`    INTEGER NOT NULL ,
-  `likes`    INTEGER NOT NULL ,
-  `dislikes` INTEGER NOT NULL ,
+  `name`     VARCHAR(100) NOT NULL UNIQUE ,
+  `URL`      VARCHAR(100) NOT NULL UNIQUE ,
+  `count`    INTEGER NOT NULL DEFAULT 0,
 
   PRIMARY KEY (`id`)
 );
@@ -53,11 +46,14 @@ CREATE TABLE `promo`.`shops`
 CREATE TABLE `promo`.`users`
 (
   `id`       INTEGER NOT NULL AUTO_INCREMENT ,
-  `login`    VARCHAR(177) NOT NULL ,
-  `email`    VARCHAR(255) NOT NULL ,
+  `login`    VARCHAR(177) NOT NULL UNIQUE,
+  `email`    VARCHAR(255) NOT NULL UNIQUE,
   `password` VARCHAR(100) NOT NULL ,
-  `rating`   INTEGER NOT NULL ,
-  `groupID`  INTEGER NOT NULL ,
+  `vkID`      INTEGER UNIQUE,
+  `rating`   INTEGER NOT NULL DEFAULT 0,
+  `session`   VARCHAR(40) NULL,
+  `lastLogin` DATETIME NULL,
+  `groupID`  INTEGER NOT NULL,
 
   PRIMARY KEY (`id`),
   FOREIGN KEY (`groupID`) REFERENCES `groups` (`id`)
@@ -90,10 +86,8 @@ CREATE TABLE `promo`.`promocodes`
   `PromoID`     INTEGER NOT NULL AUTO_INCREMENT ,
   `promo`       VARCHAR(100) ,
   `issueDate`   DATE NOT NULL ,
-  `expireDate`  DATE NOT NULL ,
-  `likes`       INTEGER NOT NULL ,
-  `dislikes`    INTEGER NOT NULL ,
-  `description` MEDIUMTEXT NOT NULL ,
+  `expireDate`  DATE NULL ,
+  `description` MEDIUMTEXT NULL ,
   `userID`      INTEGER NOT NULL ,
   `shopId`      INTEGER NOT NULL ,
 
@@ -117,4 +111,61 @@ CREATE TABLE `promo`.`promoComments`
   PRIMARY KEY (`id`),
   FOREIGN KEY (`userID`) REFERENCES `users` (`id`),
   FOREIGN KEY (`promoID`) REFERENCES `promocodes` (`PromoID`)
+);
+
+-- ************************************** `shopDislikes`
+
+CREATE TABLE `promo`.`shopDislikes`
+(
+ `id`     INTEGER NOT NULL AUTO_INCREMENT ,
+ `userID` INTEGER NOT NULL ,
+ `shopID` INTEGER NOT NULL ,
+
+PRIMARY KEY (`id`),
+FOREIGN KEY (`userID`) REFERENCES `users` (`id`),
+FOREIGN KEY (`shopID`) REFERENCES `shops` (`id`)
+);
+
+
+
+-- ************************************** `shopLikes`
+
+CREATE TABLE `promo`.`shopLikes`
+(
+ `id`     INTEGER NOT NULL AUTO_INCREMENT ,
+ `userID` INTEGER NOT NULL ,
+ `shopID` INTEGER NOT NULL ,
+
+PRIMARY KEY (`id`),
+FOREIGN KEY (`userID`) REFERENCES `users` (`id`),
+FOREIGN KEY (`shopID`) REFERENCES `shops` (`id`)
+);
+
+
+-- ************************************** `promoDislikes`
+
+CREATE TABLE `promo`.`promoDislikes`
+(
+ `id`      INTEGER NOT NULL AUTO_INCREMENT ,
+ `PromoID` INTEGER NOT NULL ,
+ `userID`  INTEGER NOT NULL ,
+
+PRIMARY KEY (`id`),
+FOREIGN KEY (`PromoID`) REFERENCES `promocodes` (`PromoID`),
+FOREIGN KEY (`userID`) REFERENCES `users` (`id`)
+);
+
+
+
+-- ************************************** `promoLikes`
+
+CREATE TABLE `promo`.`promoLikes`
+(
+ `id`      INTEGER NOT NULL AUTO_INCREMENT ,
+ `PromoID` INTEGER NOT NULL ,
+ `userID`  INTEGER NOT NULL ,
+
+PRIMARY KEY (`id`),
+FOREIGN KEY (`PromoID`) REFERENCES `promocodes` (`PromoID`),
+FOREIGN KEY (`userID`) REFERENCES `users` (`id`)
 );
